febafcc39e08ab6022809473cfcd14c00bbd2ba2
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-02-02 21:37:03 -0800

    Feature: Enable custom router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { ConstructionSign } from '@onetoo-dot-us/react-library';
 import { useContext } from 'react';
 import './App.css';
-import { RoutingContext } from './contexts/Routing';
+import { pagesMapping, RoutingContext } from './contexts/Routing';
+import Home from './pages/Home/Home';
+import Repositories from './pages/Repositories/Repositories';
 
 function App() {
   const { page } = useContext(RoutingContext);
 
   return (
     <>
-      {/* {pagesMapping.home === page && <Home />} */}
-      {/* {pagesMapping.repositories === page && <Repositories />}{' '} */}
       <ConstructionSign />
       <h1>Onetoo</h1>
+      {pagesMapping.home === page && <Home />}
+      {pagesMapping.repositories === page && <Repositories />}{' '}
     </>
   );
 }
