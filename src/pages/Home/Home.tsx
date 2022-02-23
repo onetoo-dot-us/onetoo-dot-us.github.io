6e028667b537f7780f55237243e2ecf3a15ca9bc
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-02-23 05:15:40 -0800

    Closes #4

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,8 +9,19 @@ import { www } from './www';
  */
 const Home = () => {
   return (
-    <div className='home--container'>
-      <Www what={www.what} />
+    <div className='home--div'>
+      <p className='home--p'>
+        Onetoo is an organization of whatever{' '}
+        <a
+          href='https://carlcidromero.github.io'
+          target='_blank'
+          rel='noreferrer'
+        >
+          cidro12
+        </a>{' '}
+        has been up to lately-- typically making beats, playing Rocket League
+        and coding.
+      </p>
     </div>
   );
 };
