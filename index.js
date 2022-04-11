a1e927c591a95d6f385259d560622732703a2587
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-04-10 23:19:29 -0700

    Refactor index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 (function createReusableHeader() {
-  const root = document.querySelector("div#root");
-  const header = document.createElement("header");
-  const h1 = document.createElement("h1");
+  const root = findRoot();
+  const { anchor, h1, header } = createElements();
 
-  const anchor = document.createElement("a");
   anchor.href = "/";
   anchor.textContent = "Onetoo";
 
-  h1.append(anchor);
-  header.append(h1);
   root.append(header);
+  header.append(h1);
+  h1.append(anchor);
 })();
+
+function findRoot() {
+  return document.querySelector("div#root");
+}
+
+function createElements() {
+  const anchor = document.createElement("a");
+  const h1 = document.createElement("h1");
+  const header = document.createElement("header");
+
+  return { anchor, h1, header };
+}
