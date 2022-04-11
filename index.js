860dc5341c27eda27778de60b327ccb2e30b10ff
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-04-10 22:49:10 -0700

    Implement div#root

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 document.title = "onetoo.us";
+
+const root = document.querySelector("div#root");
+const header = document.createElement("header");
+const h1 = document.createElement("h1");
+
+h1.textContent = "Onetoo";
+
+header.append(h1);
+root.append(header);
