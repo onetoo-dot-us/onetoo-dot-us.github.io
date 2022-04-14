f183188305275c3ee790ccf9e7a98a157000e750
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-04-14 09:11:34 -0700

    Render OnetooRepository.name in h2

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-class OnetooRepository {}
+class OnetooRepository extends HTMLElement {
+  static get tag() {
+    return "onetoo-repository";
+  }
+
+  constructor() {
+    super();
+
+    const h2 = document.createElement("h2");
+    h2.textContent = this.name;
+
+    this.append(h2);
+  }
+
+  get name() {
+    return this.getAttribute("name");
+  }
+}
+
+customElements.define(OnetooRepository.tag, OnetooRepository);
 
 module.exports = OnetooRepository;
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const OnetooRepository = require("./index");
 
 describe("onetoo-repository", () => {
-  it("builds", () => {
-    expect(new OnetooRepository()).toBeTruthy();
+  function findComponent() {
+    return document.body.querySelector("onetoo-repository");
+  }
+
+  it("exists in custom element registry", () => {
+    expect(customElements.get(OnetooRepository.tag)).toBeTruthy();
+  });
+
+  describe("child h2", () => {
+    it("exists", () => {
+      const component = new OnetooRepository();
+      const h2 = component.querySelector("h2");
+
+      expect(h2).toBeTruthy();
+    });
+
+    it("renders 'name' attribute", () => {
+      const name = "hello";
+
+      document.body.innerHTML = `<onetoo-repository name=${name}></onetoo-repository>`;
+
+      const component = findComponent();
+
+      expect(component.querySelector("h2").textContent).toEqual("hello");
+    });
   });
 });
