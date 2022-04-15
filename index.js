7a199226892ac639d77989eee145b6d7db1ae7b6
Carl 'Cid' Romero <carlcidromero@gmail.com>
2022-04-14 17:43:33 -0700

    Setup es6 modules

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-class OnetooRepository extends HTMLElement {
-  static get tag() {
-    return "onetoo-repository";
-  }
-
-  constructor() {
-    super();
-
-    const h2 = document.createElement("h2");
-    h2.textContent = this.name;
-
-    this.append(h2);
-  }
-
-  get name() {
-    return this.getAttribute("name");
-  }
-}
-
-customElements.define(OnetooRepository.tag, OnetooRepository);
-
-module.exports = OnetooRepository;
+export * from "./components/onetoo-repository/onetoo-repository";
+export * from "./components/onetoo-dl/onetoo-dl";
